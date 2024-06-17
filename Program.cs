5d56d2d53125d2bf9572058a0e5a0dd732af6c6e
ivan montañes <ivanmontanesfilipit@gmail.com>
2024-06-16 21:49:56 -0300

    empleados inicializados

--- Program.cs
+++ Program.cs
@@ -1,13 +1,26 @@
 using EspacioCalculadora;
-Empleado NuevoEmpleado = new Empleado();
-NuevoEmpleado.nombre = "ivan";
-NuevoEmpleado.apellido = "montañes";
-NuevoEmpleado.FechaNacimiento = new DateTime(2005,1,27);
-NuevoEmpleado.estadoCivil = 'c';
-NuevoEmpleado.fechaIngreso = new DateTime(2019,6,15);
-NuevoEmpleado.sueldoBasico = 100000;
-NuevoEmpleado.cargo = Cargos.Ingeniero;
-NuevoEmpleado.antiguedad();
-NuevoEmpleado.edad();
-NuevoEmpleado.jubilacion();
-NuevoEmpleado.calcularSalario();
+Empleado[] empleados = new Empleado[3];
+//empleado 1//
+empleados[0].nombre = "bruno";
+empleados[0].apellido = "galvan";
+empleados[0].cargo = Cargos.Administrativo;
+empleados[0].estadoCivil = 's';
+empleados[0].fechaIngreso = new DateTime(2015,3,15);
+empleados[0].FechaNacimiento = new DateTime(2004,10,21);
+empleados[0].sueldoBasico = 1100000;
+//empleado 2//
+empleados[0].nombre = "santiago";
+empleados[0].apellido = "gonzalez";
+empleados[0].cargo = Cargos.Ingeniero;
+empleados[0].estadoCivil = 's';
+empleados[0].fechaIngreso = new DateTime(2010,6,2);
+empleados[0].FechaNacimiento = new DateTime(2005,2,14);
+empleados[0].sueldoBasico = 1350000;
+//empleado 3//
+empleados[0].nombre = "vicente";
+empleados[0].apellido = "alzogaray";
+empleados[0].cargo = Cargos.Auxiliar;
+empleados[0].estadoCivil = 'c';
+empleados[0].fechaIngreso = new DateTime(2021,5,5);
+empleados[0].FechaNacimiento = new DateTime(2005,4,10);
+empleados[0].sueldoBasico = 550000;
